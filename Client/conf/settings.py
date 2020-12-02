7abacd1fd2a4cf0cfc91ab3749ca767695d91b2d
shunchen <18870257397@163.com>
2020-12-02 18:23:46 +0800

    Revert "Revert "千金难买爷乐意。冲刺吧陈司机。""
    This reverts commit 35c27b770c58b6a3f4b95d9b57d93d160e364352.

---
+++ Client/conf/settings.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+import os
+
+# 远端接收数据的服务器
+Params = {
+    "server": "10.254.3.250",
+    "port": 8000,
+    'url': '/assets/report/',
+    'request_timeout': 30,
+}
+
+# 日志文件配置
+
+PATH = os.path.join(os.path.dirname(os.getcwd()), 'log', 'cmdb.log')
